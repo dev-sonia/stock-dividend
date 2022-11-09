928b69ef5ae070561876db4c97842de167c9cd4c
dalgorithm <llsa0927@naver.com>
2022-11-09 23:05:05 +0900

    feat: 회원가입 with JWT

---
+++ src/main/java/com/example/dividend/model/constraints/Authority.java
@@ -0,0 +1,6 @@
+package com.example.dividend.model.constraints;
+
+public enum Authority {
+    ROLE_READ,
+    ROLE_WRITE;
+}
---
+++ src/main/java/com/example/dividend/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.example.dividend.service;
+
+import com.example.dividend.model.Auth;
+import com.example.dividend.persist.MemberRepository;
+import com.example.dividend.persist.entity.MemberEntity;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class MemberService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("could not find user ->" + username));
+    }
+
+    public MemberEntity register(Auth.SignUp member) {
+        boolean exists = this.memberRepository.existsByUsername(member.getUsername());
+        if (exists) {
+            throw new RuntimeException("이미 사용 중인 아이디 입니다.");
+        }
+
+        member.setPassword(this.passwordEncoder.encode(member.getPassword()));
+
+        return this.memberRepository.save(member.toEntity());
+    }
+
+    public MemberEntity authenticate(Auth.SignIn member) {
+        return null;
+    }
+}
